d4d8877c45f866e963e91b302dcd6967341a1a72
loneyclown <40462995@qq.com>
2024-06-29 03:25:32 +0800

    修复链接绑定，去除抽卡默认统计的4星，后续会加入配置并对接锅巴。

--- apps/gacha/index.ts
+++ apps/gacha/index.ts
@@ -72,6 +72,7 @@ export default class App extends Plugin {
     }
     util.writeJSON(this.jsonPath, links)
     this.e.reply('抽卡链接绑定成功！')
+    this.finish(this.vGachaBindLink.name)
     return true
   }
 
@@ -95,7 +96,7 @@ export default class App extends Plugin {
     }
     const type = this.e.msg.includes('常驻') ? '常驻' : 'UP'
     const gcm = new GaChaModel(user_id)
-    const gachaData = gcm.getGachaData(type, true)
+    const gachaData = gcm.getGachaData(type, false)
     const img = await image.createPage<GaChaAppProps>(user_id, 'gacha/index', {
       roleData: gachaData.role,
       weaponData: gachaData.weapon,
